6f1069298f743ea4c7e194dbcf0f1fb683259611
Ravenant1234 <69699906+Ravenant1234@users.noreply.github.com>
2022-08-01 00:26:00 -0300

    Update MessageBox.java

--- src/roguelike/logic/text/MessageBox.java
+++ src/roguelike/logic/text/MessageBox.java
@@ -4,37 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MessageBox {
-
 	private List<String> messageQueue;
 	private List<Integer> times;
 	
 	public MessageBox() {
-		this.messageQueue = new ArrayList<>();
-		this.times = new ArrayList<>();
+		messageQueue = new ArrayList<>();
+		times = new ArrayList<>();
 	}
 	
 	public void addMessage(String text, int time) {
-		this.messageQueue.add(text);
-		this.times.add(time*10);
+		messageQueue.add(text);
+		times.add(time*10);
 	}
 	
 	public String getMessage() {
 		try {
-			this.times.set(0, this.times.get(0)-1);
-			
-			if(this.times.get(0) <= 0) {
-				this.times.remove(0);
-				this.messageQueue.remove(0);
+			times.set(0, this.times.get(0)-1);
+			if(times.get(0) <= 0) {
+				times.remove(0);
+				messageQueue.remove(0);
 			}
-			
-			return this.messageQueue.get(0);
-			
+			return messageQueue.get(0);
 		} catch(IndexOutOfBoundsException e) {
 			return null;
 		}
 	}
 	
 	public int getTime() {
-		return this.times.get(0);
+		return times.get(0);
 	}
 }
