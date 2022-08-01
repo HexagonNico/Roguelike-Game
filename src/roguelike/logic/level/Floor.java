39ca4dcc42b7e684febbd932181f060d849139b9
Ravenant1234 <69699906+Ravenant1234@users.noreply.github.com>
2022-08-01 00:29:58 -0300

    Optimized code

--- src/roguelike/logic/level/Floor.java
+++ src/roguelike/logic/level/Floor.java
@@ -6,10 +6,16 @@ import java.util.List;
 import roguelike.logic.entities.Monster;
 
 public class Floor {
-
 	private Tile[][] floor;
-	
+
+	/**
+	 * Posicion de arranque en el eje x
+	 */
 	private int startPosX;
+
+	/**
+	 * Posicion de arranque en el eje y
+	 */
 	private int startPosY;
 	
 	private List<Monster> monsters;
@@ -22,63 +28,27 @@ public class Floor {
 	
 	public Floor(String[] levelData, int startPosX, int startPosY, boolean isDark, Monster... monsters) {
 		floor = new Tile[levelData.length][];
-		
 		for(int y=0;y<levelData.length;y++) {
 			floor[y] = new Tile[levelData[y].length()];
-			
 			for(int x=0;x<levelData[y].length();x++) {
-				switch(levelData[y].charAt(x)) {
-				case '#':
-					floor[y][x] = new Tile("wall", x, y);
-					break;
-				case '.':
-					floor[y][x] = new Tile("floor", x, y);
-					break;
-				case '^':
-					floor[y][x] = new Tile("stairs", x, y);
-					break;
-				case ',':
-					floor[y][x] = new Tile("trap", x, y);
-					break;
-				case 'p':
-					floor[y][x] = new Tile("red_potion", x, y);
-					break;
-				case 't':
-					floor[y][x] = new Tile("table", x, y);
-					break;
-				case 'G':
-					floor[y][x] = new Tile("gold_bag", x, y);
-					break;
-				case '!':
-					floor[y][x] = new Tile("key", x, y);
-					break;
-				case 's':
-					floor[y][x] = new Tile("purple_potion", x, y);
-					break;
-				case 'v':
-					floor[y][x] = new Tile("green_potion", x, y);
-					break;
-				case 'd':
-					floor[y][x] = new Tile("lime_potion", x, y);
-					break;
-				case 'c':
-					floor[y][x] = new Tile("drawer", x, y);
-					break;
-				case 'g':
-					floor[y][x] = new Tile("yellow_potion", x, y);
-					break;
-				case 'e':
-					floor[y][x] = new Tile("table2", x, y);
-					break;
-				case '/':
-					floor[y][x] = new Tile("locked_door", x, y);
-					break;
-				case 'T':
-					floor[y][x] = new Tile("chest", x, y);
-					break;
-				case 'l':
-					floor[y][x] = new Tile("torch", x, y);
-					break;
+				switch (levelData[y].charAt(x)) {
+					case '#' -> floor[y][x] = new Tile("wall", x, y);
+					case '.' -> floor[y][x] = new Tile("floor", x, y);
+					case '^' -> floor[y][x] = new Tile("stairs", x, y);
+					case ',' -> floor[y][x] = new Tile("trap", x, y);
+					case 'p' -> floor[y][x] = new Tile("red_potion", x, y);
+					case 't' -> floor[y][x] = new Tile("table", x, y);
+					case 'G' -> floor[y][x] = new Tile("gold_bag", x, y);
+					case '!' -> floor[y][x] = new Tile("key", x, y);
+					case 's' -> floor[y][x] = new Tile("purple_potion", x, y);
+					case 'v' -> floor[y][x] = new Tile("green_potion", x, y);
+					case 'd' -> floor[y][x] = new Tile("lime_potion", x, y);
+					case 'c' -> floor[y][x] = new Tile("drawer", x, y);
+					case 'g' -> floor[y][x] = new Tile("yellow_potion", x, y);
+					case 'e' -> floor[y][x] = new Tile("table2", x, y);
+					case '/' -> floor[y][x] = new Tile("locked_door", x, y);
+					case 'T' -> floor[y][x] = new Tile("chest", x, y);
+					case 'l' -> floor[y][x] = new Tile("torch", x, y);
 				}
 			}
 		}
@@ -103,12 +73,10 @@ public class Floor {
 			}
 		}
 		
-		this.startPosX = copy.getStartPosX();
-		this.startPosY = copy.getStartPosY();
-		
-		this.monsters = copy.getMonstersList();
-		
-		this.dark = copy.isDark();
+		startPosX = copy.getStartPosX();
+		startPosY = copy.getStartPosY();
+		monsters = copy.getMonstersList();
+		dark = copy.isDark();
 	}
 	
 	public int getSizeX() {
@@ -133,19 +101,17 @@ public class Floor {
 	
 	public Monster[] getMonsters() {
 		Monster[] other = new Monster[monsters.size()];
-		other = monsters.toArray(other);
-		return other;
+		return monsters.toArray(other);
 	}
 	
 	private List<Monster> getMonstersList(){
-		return this.monsters;
+		return monsters;
 	}
 	
 	public Monster getMonsterAt(int x, int y) {
 		for(Monster monster : monsters) {
 			if(monster == null)
 				return null;
-			
 			if(monster.getPosX() == x && monster.getPosY() == y)
 				return monster;
 		}
@@ -162,7 +128,7 @@ public class Floor {
 	 * @return True if a trap at (x;y) was disarmed, false if there was no trap there
 	 */
 	public boolean disarmTrap(int x, int y) {
-		if(floor[y][x].getName() == "trap") {
+		if(floor[y][x].getName().equals("trap")) {
 			floor[y][x] = new Tile("floor", x, y);
 			return true;
 		}
@@ -175,59 +141,48 @@ public class Floor {
 	 * @return True if a collectible at (x;y) was removed, false if there was no collectible there
 	 */
 	public boolean removeCollectible(int x, int y) {
-		switch(floor[y][x].getName()) {
-		case "red_potion":
-		case "key":
-		case "lime_potion":
-		case "green_potion":
-			floor[y][x] = new Tile("table", x, y);
-			return true;
-		case "gold_bag":
-		case "purple_potion":
-			floor[y][x] = new Tile("drawer", x, y);
-			return true;
-		case "yellow_potion":
-			floor[y][x] = new Tile("table2", x, y);
-			return true;
-		case "chest":
-			floor[y][x] = new Tile("open_chest", x, y);
-			return true;
+		switch (floor[y][x].getName()) {
+			case "red_potion", "key", "lime_potion", "green_potion" -> floor[y][x] = new Tile("table", x, y);
+			case "gold_bag", "purple_potion" -> floor[y][x] = new Tile("drawer", x, y);
+			case "yellow_potion" -> floor[y][x] = new Tile("table2", x, y);
+			case "chest" -> floor[y][x] = new Tile("open_chest", x, y);
+			default -> {
+				return false;
+			}
 		}
-		return false;
+		return true;
 	}
 	
 	/**Turns a door tile into a floor tile
 	 * @param x - X coordinate of door
 	 * @param y - Y coordinate of door
-	 * @return True if a door at (x;y) was opened, false if there was no door there
 	 */
-	public boolean openDoor(int x, int y) {
-		if(floor[y][x].getName() == "locked_door") {
+	public void openDoor(int x, int y) {
+		if(floor[y][x].getName().equals("locked_door"))
 			floor[y][x] = new Tile("floor", x, y);
-			return true;
-		}
-		return false;
 	}
 	
-	/**Deletes a monster
+	/**
+	 * Deletes a monster
+	 *
 	 * @param x - X coordinate of monster
 	 * @param y - Y coordinate of monster
-	 * @return True if a monster at (x;y) was killed, false if there was no monster there
 	 */
-	public boolean killMonster(int x, int y) {
+	public void killMonster(int x, int y) {
 		for(int i=0;i<monsters.size();i++) {
 			if(monsters.get(i).getPosX() == x && monsters.get(i).getPosY() == y) {
 				monsters.remove(i);
 				System.out.println("[GameLogic][Floor]: Monster killed");
-				return true;
+				return;
 			}
 		}
-		return false;
 	}
+
+
 	
 	public boolean thereIsMonsterHere(int x, int y) {
-		for(int i=0;i<monsters.size();i++) {
-			if(monsters.get(i).getPosX() == x && monsters.get(i).getPosY() == y)
+		for (Monster monster : monsters) {
+			if (monster.getPosX() == x && monster.getPosY() == y)
 				return true;
 		}
 		return false;
